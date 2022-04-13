62a274ab4d3ee68bd1cf5784bfe8af9a69fb33df
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2022-04-13 23:07:45 +0400

    feat: fetch data for detail page

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -1,26 +1,56 @@
 import { Fragment } from 'react';
+
+import Image from 'next/image';
 import Layout from '../components/Layout/Layout';
 import Card from '../components/UI/Card';
 import Container from '../components/Layout/components/Container';
 
-const MeetupDetails = () => {
+export const getStaticProps = async (context) => {
+    const meetupId = context.params.meetupId;
+    // fetch data
+    const response = await fetch(
+        `http://127.0.0.1:8000/api/meetups/${meetupId}`
+    );
+    const data = await response.json();
+
+    return {
+        props: {
+            meetups: data,
+        },
+    };
+};
+
+export const getStaticPaths = async () => {
+    return {
+        paths: [],
+        fallback: 'blocking',
+    };
+};
+
+const MeetupDetails = ({ meetups }) => {
     return (
         <Fragment>
             <Layout />
             <Container className={'max-w-2xl'}>
                 <Card>
                     <div>
-                        <img
+                        <Image
                             className='rounded-t w-full object-cover'
-                            src='https://images.unsplash.com/photo-1497215728101-856f4ea42174?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
-                            alt='title'
+                            src={meetups.image}
+                            alt={meetups.title}
+                            layout={'responsive'}
+                            priority
+                            width={800}
+                            height={500}
                         />
                     </div>
                     <div className='p-5 text-center space-y-3 mb-5'>
-                        <h1 className='text-3xl text-main'>Meetup Title</h1>
-                        <p className='italic text-xl text-sm'>Meetup address</p>
+                        <h1 className='text-3xl text-main'>{meetups.title}</h1>
+                        <p className='italic text-xl text-sm'>
+                            {meetups.address}
+                        </p>
                         <p className='w-1/2 m-auto text-main border border-main p-5 rounded'>
-                            Meetup description
+                            {meetups.description}
                         </p>
                     </div>
                 </Card>
